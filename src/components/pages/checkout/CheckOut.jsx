ead04214785f37ffd10a7192883de3c1acbdc719
Intime-SA <invertimesa@gmail.com>
2023-09-02 15:33:23 -0300

    prueba

--- src/components/pages/checkout/CheckOut.jsx
+++ src/components/pages/checkout/CheckOut.jsx
@@ -62,7 +62,7 @@ function CheckOut() {
     });
     try {
       let res = await axios.post(
-        "https://nueva-ap-pduje.vercel.app/create_preference",
+        "https://nueva-ap-pduje-vblc.vercel.app/create_preference",
         {
           items: newArray,
           shipment_cost: 10,
