537e7b5f923031894df95342fa8a43fcddeaf27d
Intime-SA <invertimesa@gmail.com>
2024-06-19 02:48:29 -0300

    correccion de cambios

--- src/router/routes.js
+++ src/router/routes.js
@@ -58,7 +58,7 @@ export const routes = [
   },
   {
     id: "checkout-success",
-    path: "/checkout-success/:numberOrder/status=approved",
+    path: "/checkout-success/:numberOrder/:status",
     Element: CheckOutSuccess,
   },
 ];
